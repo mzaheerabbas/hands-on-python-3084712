ee54a8e39cbe0f98455fc8b44073aeca23287910
zaheer <mzaheerabbas@users.noreply.github.com>
2024-07-21 20:16:41 +0000

    comit this

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -3,4 +3,4 @@ import requests
 response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
 
-last_twenty_years = response.json()[1][:20]
+last_twenty_years = response.json()[1][:21]
